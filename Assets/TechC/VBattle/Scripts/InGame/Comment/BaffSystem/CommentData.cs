896bd6a0ca86ea5307a4fc212a9d7a3e715c90cd
YutoImata <yuto.imata.wine@gamil.com>
2025-04-24 16:30:52 +0900

    CommentProviderとCommentDataを分離

---
+++ Assets/TechC/VBattle/Scripts/InGame/Comment/BaffSystem/CommentData.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TechC
+{
+   /// <summary>
+    /// コメントタイプを定義
+    /// </summary>
+    public enum CommentType { Normal, Buff, MapChange }
+
+    public class CommentData
+    {
+        public CommentType type;
+        public string text;
+        public BuffType? buffType;
+
+        /* コンストラクタでコメントタイプ、テキスト、バフタイプを設定 */
+        public CommentData(CommentType type, string text, BuffType? buffType)
+        {
+            this.type = type;
+            this.text = text;
+            this.buffType = buffType;
+        }
+    }
+}
--- Assets/TechC/VBattle/Scripts/InGame/Comment/CommentSystem/CommentProvider.cs
+++ Assets/TechC/VBattle/Scripts/InGame/Comment/CommentSystem/CommentProvider.cs
@@ -4,25 +4,6 @@ using UnityEngine;
 namespace TechC
 {
     /// <summary>
-    /// コメントタイプを定義
-    /// </summary>
-    public enum CommentType { Normal, Buff, MapChange }
-
-    public class CommentData
-    {
-        public CommentType type;
-        public string text;
-        public BuffType? buffType;
-
-        /* コンストラクタでコメントタイプ、テキスト、バフタイプを設定 */
-        public CommentData(CommentType type, string text, BuffType? buffType)
-        {
-            this.type = type;
-            this.text = text;
-            this.buffType = buffType;
-        }
-    }
-    /// <summary>
     /// ランダムにコメントを選び提供する
     /// </summary>
     public class CommentProvider : MonoBehaviour

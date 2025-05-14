6dc48de831a24885570d214836fa82d36bf50fc2
YutoImata <yuto.imata.wine@gmail.com>
2025-05-14 14:15:33 +0900

    バフの種類によってエフェクトが変わる処理を追加

--- Assets/TechC/VBattle/Scripts/InGame/System/EffectFactory.cs
+++ Assets/TechC/VBattle/Scripts/InGame/System/EffectFactory.cs
@@ -19,18 +19,12 @@ namespace TechC
         /// </summary>
         /// <param name="effectName">エフェクト名</param>
         /// <param name="position">エフェクトの位置</param>
-        /// <param name="autoReturnTime">自動返却までの時間（省略可）</param>
-        public void PlayEffect(string effectName, int playerID,Quaternion rotation, float autoReturnTime = 0f)
+        /// <param name="effectRemainingTime">自動返却までの時間（省略可）</param>
+        public void PlayEffect(string effectName, int playerID,Quaternion rotation, float effectRemainingTime = 0f)
         {
             /* ObjectPoolから指定された名前のエフェクトを取得 */
             GameObject effect = effectPool.GetObjectByName(effectName);
-            Debug.Log("エフェクトのPrefabを適用メソッド呼ばれた");
-
-            if (effect == null)
-            {
-                Debug.LogWarning($"エフェクト{effectName}が見つかりません");
-                return;
-            }
+            
 
             /* エフェクトの位置を回転を設定 */
             // effect.transform.position = position; /* 位置を設定 */
@@ -41,43 +35,11 @@ namespace TechC
             effect.SetActive(true); /* エフェクトを表示 */
             Debug.Log("エフェクトのPrefabを適用");
 
-            if (autoReturnTime > 0f)
+            if (effectRemainingTime > 0f)
             {
-                StartCoroutine(AutoReturn(effect, autoReturnTime));
+                StartCoroutine(AutoReturn(effect, effectRemainingTime));
             }
         }
-        /// <summary>
-        /// 位置だけを指定してエフェクトを再生（回転はデフォルト値）
-        /// </summary>
-        /// <param name="effectName">エフェクト名</param>
-        /// <param name="position">エフェクトの位置</param>
-        /// <param name="autoReturnTime">自動返却までの時間（省略可）</param>
-        public void PlayEffect(string effectName, Vector3 position, Quaternion rotation, float autoReturnTime = 0f)
-        {
-            /* ObjectPoolから指定された名前のエフェクトを取得 */
-            GameObject effect = effectPool.GetObjectByName(effectName);
-            Debug.Log("エフェクトのPrefabを適用メソッド呼ばれた");
-
-            if (effect == null)
-            {
-                Debug.LogWarning($"エフェクト{effectName}が見つかりません");
-                return;
-            }
-
-            /* エフェクトの位置を回転を設定 */
-            effect.transform.position = position; /* 位置を設定 */
-            effect.transform.rotation = rotation; /* 回転を設定 */
-
-            effect.SetActive(true); /* エフェクトを表示 */
-            Debug.Log("エフェクトのPrefabを適用");
-
-            if (autoReturnTime > 0f)
-            {
-                StartCoroutine(AutoReturn(effect, autoReturnTime));
-            }
-        }
-
-
 
         /// <summary>
         /// 指定時間後にエフェクトをプールに返却する

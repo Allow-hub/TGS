3ebd33589a55f7a03d6429c342910a1726b4811d
YutoImata <yuto.imata.wine@gmail.com>
2025-05-24 16:25:46 +0900

    弱攻撃でSummaryは

--- Assets/TechC/VBattle/Scripts/InGame/Player/Terami/Terami_WeakAttack.cs
+++ Assets/TechC/VBattle/Scripts/InGame/Player/Terami/Terami_WeakAttack.cs
@@ -9,27 +9,43 @@ namespace TechC
     /// </summary>
     public class Terami_WeakAttack : WeakAttack
     {
+        /// <summary>
+        /// ゴムベラを前に振る、前方への軽い攻撃。3回目で派生
+        /// </summary>
         public override void NeutralAttack()
         {
             base.NeutralAttack();
 
         }
 
+        /// <summary>
+        /// 前方をロールして相手を蹴る
+        /// </summary>
         public override void LeftAttack()
         {
             base.LeftAttack();
         }
 
+        /// <summary>
+        /// 高速で後ろに下がる
+        /// </summary>
         public override void RightAttack()
         {
             base.RightAttack();
         }
+
+        /// <summary>
+        /// 地面を叩いて、周囲にダメージを与える
+        /// </summary>
 
         public override void DownAttack()
         {
             base.DownAttack();
         }
 
+        /// <summary>
+        /// 相手の上部からお菓子を落として攻撃
+        /// </summary>
         public override void UpAttack()
         {
             base.UpAttack();

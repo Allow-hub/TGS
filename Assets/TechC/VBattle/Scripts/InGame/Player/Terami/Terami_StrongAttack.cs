250551ce9cbbd6621782edb2c2d0bbd451003474
YutoImata <yuto.imata.wine@gmail.com>
2025-05-31 13:29:14 +0900

    Summaryを追加

--- Assets/TechC/VBattle/Scripts/InGame/Player/Terami/Terami_StrongAttack.cs
+++ Assets/TechC/VBattle/Scripts/InGame/Player/Terami/Terami_StrongAttack.cs
@@ -9,27 +9,50 @@ namespace TechC
     /// </summary>
     public class Terami_StrongAttack : StrongAttack
     {
+        // [Header("プレハブの参照")]
+        // [Header("ニュートラル強")]
+        // [Header("左強")]
+        // [Header("右強")]
+        // [Header("下強")]
+        // [Header("上強")]
+
+
+        /// <summary>
+        /// 回復アイテムをキャラ２の目の前に出す、一定期間経過後再利用可能
+        /// </summary>
         public override void NeutralAttack()
         {
             base.NeutralAttack();
 
         }
 
+        /// <summary>
+        /// 未定
+        /// </summary>
         public override void LeftAttack()
         {
             base.LeftAttack();
         }
 
+        /// <summary>
+        /// 未定
+        /// </summary>
         public override void RightAttack()
         {
             base.RightAttack();
         }
 
+        /// <summary>
+        /// 未定
+        /// </summary>
         public override void DownAttack()
         {
             base.DownAttack();
         }
 
+        /// <summary>
+        /// 大きさ、攻撃力、当たり判定が２倍になり、移動速度が1/2になる
+        /// </summary>
         public override void UpAttack()
         {
             base.UpAttack();

231a6dbc9cc86c07e297a6f88dadc22788f7c01b
SHIMAMOTO ZENTA <SBA24J10004@edu.sba.ac.jp>
2025-06-04 15:09:25 +0900

    音データ１

--- Assets/TechC/VBattle/Scripts/ManagerScene/GameManager.cs
+++ Assets/TechC/VBattle/Scripts/ManagerScene/GameManager.cs
@@ -26,6 +26,7 @@ namespace TechC
             QualitySettings.vSyncCount = 0;
             // fps 144 を目標に設定
             Application.targetFrameRate = targetFrameRate;
+            AudioManager.I.PlayBGM(BGMID.Title);
         }
 
 

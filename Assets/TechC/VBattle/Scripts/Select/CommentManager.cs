a25f41c074f145ff820a135c4d2b182247b38d6f
SHIMAMOTO ZENTA <SBA24J10004@edu.sba.ac.jp>
2025-07-02 17:48:09 +0900

    select画面の調整、メニュー画面に入れるための画像の追加

---
+++ Assets/TechC/VBattle/Scripts/Select/CommentManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommentManager : MonoBehaviour
+{
+    public List<string> commentList = new List<string>();
+
+    public GameObject commentPrefab;
+    public RectTransform commentParent;
+
+    public float spawnInterval = 1.0f;
+
+    // マジックナンバー → SerializeFieldでインスペクタから編集可能に
+    [SerializeField] private float minYPosition = -50f;
+    [SerializeField] private float maxYPosition = 50f;
+    [SerializeField] private float minSpeed = 150f;
+    [SerializeField] private float maxSpeed = 250f;
+
+    void Start()
+    {   
+        StartCoroutine(SpawnCommentsRoutine());
+    }
+
+    IEnumerator SpawnCommentsRoutine()
+    {
+        while (true)
+        {
+            if (commentList.Count > 0)
+            {
+                string message = commentList[Random.Range(0, commentList.Count)];
+                GameObject obj = Instantiate(commentPrefab, commentParent);
+                float yPos = Random.Range(minYPosition, maxYPosition);
+                float speed = Random.Range(minSpeed, maxSpeed);
+
+                obj.GetComponent<CommentMover>().Initialize(message, speed, yPos);
+            }
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}

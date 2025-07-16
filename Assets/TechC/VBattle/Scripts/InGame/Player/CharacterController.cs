c67529774c50ae1f6c1e836382a4212b77d9980f
22RENT-16\student <y24340216@gmail.com>
2025-07-16 16:57:43 +0900

    ガードの分離

--- Assets/TechC/VBattle/Scripts/InGame/Player/CharacterController.cs
+++ Assets/TechC/VBattle/Scripts/InGame/Player/CharacterController.cs
@@ -231,9 +231,12 @@ namespace TechC.Player
             UpdateStateTransitions();
 
             lastVelocity = rb.velocity;
+            
             // ガード値回復処理
             if (CanHeal())
                 HealGuardPower(characterData.GuardRecoverySpeed);
+
+
             //Debug.Log(IsChargeEnabled());
             // 時間経過によるゲージ加算処理
             //characterGauge.AddGaugeOnTime(characterData.GaugeIncreaseInterval, characterData.GaugeIncreaseAmount);
@@ -437,69 +440,7 @@ namespace TechC.Player
         public HitData GetLastHitData() => lastHitData;
         #endregion
 
-        #region ガード関連メソッド
-        /// <summary>
-        /// ガードパワーを取得
-        /// </summary>
-        public float GetGuardPower() => currentGuardPower;
-
-        /// <summary>
-        /// ガード中に耐久値減少
-        /// </summary>
-        public void DecreaseGuardPower() => currentGuardPower -= characterData.GuardDecreasePower;
-
-        /// <summary>
-        /// 最後にガードした時間を設定
-        /// </summary>
-        public void SetLastGuardTime(float time) => lastGuardTime = time;
-
-        /// <summary>
-        /// ガード時のダメージ処理
-        /// </summary>
-        public void GuardDamage(float damage, ICommand guardCommand)
-        {
-            currentGuardPower -= damage;
-            AudioManager.I.PlayCharacterSE(characterType, CharacterSEType.Guard);
-            if (inputDevice is Gamepad gamepad)
-                GamepadVibrationUtility.Vibrate(lowFrequency, highFrequency, duration, gamepad);
-            if (currentGuardPower > 0) return;
-            currentGuardPower = 0;
-            GuardBreak(guardCommand);
-        }
-
-        /// <summary>
-        /// ガードブレイク処理
-        /// </summary>
-        public void GuardBreak(ICommand guardCommand)
-        {
-            AudioManager.I.PlayCharacterSE(characterType, CharacterSEType.GuardBreak);
-            guardCommand.ForceFinish();
-            currentGuardPower = 0; // ガードがマイナスで保存されないように
-            Debug.Log("Guardが破壊されました");
-        }
-
-        /// <summary>
-        /// ガードパワー回復処理
-        /// </summary>
-        public void HealGuardPower(float value)
-        {
-            if (currentGuardPower < characterData.GuardPower)
-                currentGuardPower += value;
-            else
-                currentGuardPower = characterData.GuardPower;
-            //Debug.Log($"{currentGuardPower}");
-        }
-
-        /// <summary>
-        /// ガード回復が可能な状態かを判定
-        /// </summary>
-        public bool CanHeal()
-        {
-            var lastGuard = Time.time - lastGuardTime;
-            return lastGuard >= characterData.GuardRecoveryInterval;
-        }
-        #endregion
-
+       
         #region 必殺技関連メソッド
         /// <summary>
         /// 必殺技ゲージを増加させる

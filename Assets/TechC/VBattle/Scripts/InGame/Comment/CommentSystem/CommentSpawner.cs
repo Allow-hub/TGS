bcb2a55266101349903c8b2ec32cba9ee90a2d28
YutoImata <yuto.imata.wine@gmail.com>
2025-07-16 14:04:26 +0900

    コメント生成処理にマテリアル適用を追加し、初期化処理を整理

--- Assets/TechC/VBattle/Scripts/InGame/Comment/CommentSystem/CommentSpawner.cs
+++ Assets/TechC/VBattle/Scripts/InGame/Comment/CommentSystem/CommentSpawner.cs
@@ -20,8 +20,8 @@ namespace TechC
         [SerializeField] private Transform bottomRightSpawnPos;
         private float topRightSpawnPosY = 5.0f;
         private float bottomRightSpawnPosY = -5.0f;
-         private float spawnPosX = 10.0f;
-        
+        private float spawnPosX = 10.0f;
+
         private const float PLAYER_TOP_OFFSET = -5.3f;
         private bool isInitialized = false;
 
@@ -30,6 +30,7 @@ namespace TechC
             topRightSpawnPosY = topRightSpawnPos.position.y;
             bottomRightSpawnPosY = bottomRightSpawnPos.position.y;
             spawnPosX = topRightSpawnPos.position.x;
+            isInitialized = true;
         }
 
         /// <summary>
@@ -38,17 +39,42 @@ namespace TechC
         /// <returns>生成されたコメントのGameObject</returns>
         public GameObject SpawnComment()
         {
+            if (!isInitialized)
+            {
+                Init();
+            }
+
             var commentData = commentProvider.GetRandomComment();
             GameObject comment = CommentFactory.I.GetComment(commentData, GetCommentPrefab(commentData));
 
+            // 特殊コメントの処理
+            var sp = comment.GetComponent<FreezeCommentTrigger>();
+            var spType = SpecialCommentChecker.GetSpecialCommentType(commentData.text);
+            Material commentMaterial;
+
+            var materialApplier = CommentDisplay.I.GetMaterialApplier();
+
+            if (spType != SpecialCommentType.None && sp != null)
+                commentMaterial = materialApplier.GetCommentMaterial(null, sp.SpecialType);
+            else
+                commentMaterial = materialApplier.GetCommentMaterial(commentData.type);
+
             List<GameObject> spawnedChars = AllCharacterHelper.ProcessCommentText(commentData.text, comment.transform, Color.white);
+            
+            // マテリアルを適用
+            materialApplier.ApplyMaterialToCharacters(spawnedChars, commentMaterial);
 
             float randomY = UnityEngine.Random.Range(bottomRightSpawnPosY, topRightSpawnPosY);
             comment.transform.position = new Vector3(spawnPosX, randomY, PLAYER_TOP_OFFSET);
 
+            var freezeCommentTrigger = comment.GetComponent<FreezeCommentTrigger>();
+
+            // 移動処理を開始
+            CommentDisplay.I.GetMover().StartMoving(comment.transform, spawnedChars, freezeCommentTrigger, commentMaterial);
+
             return comment;
         }
-        
+
         private GameObject GetCommentPrefab(CommentData commentData)
         {
             switch (commentData.type)
@@ -56,13 +82,13 @@ namespace TechC
                 case CommentType.Normal:
                     return commentPrefab;
                 case CommentType.AttackBuff:
-                    return attackBuffPrefab;   
+                    return attackBuffPrefab;
                 case CommentType.MapChange:
-                    return mapChangePrefab;    
+                    return mapChangePrefab;
                 case CommentType.SpeedBuff:
-                    return speedBuffPrefab;    
+                    return speedBuffPrefab;
                 default:
-                    return commentPrefab;      
+                    return commentPrefab;
             }
         }
     }
